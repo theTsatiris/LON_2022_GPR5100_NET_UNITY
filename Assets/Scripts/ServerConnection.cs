eb059acc9fa0d68cfd29a7e9f62add0994ba38f1
GTsatiris <george.tsatiris@gmail.com>
2022-06-16 18:14:04 +0300

    Initial commit

---
+++ Assets/Scripts/ServerConnection.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class ServerConnection : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+}
